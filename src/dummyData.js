71482e677e59b13b801abb3a991f4afff078be7a
darshit-topcodx <darshit.topcodx@gmail.com>
2023-06-19 13:22:23 +0530

    'data-changes'

--- src/dummyData.js
+++ src/dummyData.js
@@ -106,51 +106,8 @@ const templet = [
     {
         img: '',
         title: 'Nobody wants to read boring blog titles, generate catchy blog titles with this tool'
-    },
-    {
-        img: '',
-        title: 'Nobody wants to read boring blog titles, generate catchy blog titles with this tool'
-    },
-    {
-        img: '',
-        title: 'Nobody wants to read boring blog titles, generate catchy blog titles with this tool'
-    },
-    {
-        img: '',
-        title: 'Nobody wants to read boring blog titles, generate catchy blog titles with this tool'
-    },
-    {
-        img: '',
-        title: 'Nobody wants to read boring blog titles, generate catchy blog titles with this tool'
-    },
-    {
-        img: '',
-        title: 'Nobody wants to read boring blog titles, generate catchy blog titles with this tool'
-    },
-    {
-        img: '',
-        title: 'Nobody wants to read boring blog titles, generate catchy blog titles with this tool'
-    },
-    {
-        img: '',
-        title: 'Nobody wants to read boring blog titles, generate catchy blog titles with this tool'
-    },
-    {
-        img: '',
-        title: 'Nobody wants to read boring blog titles, generate catchy blog titles with this tool'
-    }, {
-        img: '',
-        title: 'Nobody wants to read boring blog titles, generate catchy blog titles with this tool'
-    }, {
-        img: '',
-        title: 'Nobody wants to read boring blog titles, generate catchy blog titles with this tool'
-    }, {
-        img: '',
-        title: 'Nobody wants to read boring blog titles, generate catchy blog titles with this tool'
-    }, {
-        img: '',
-        title: 'Nobody wants to read boring blog titles, generate catchy blog titles with this tool'
     }
+ 
 ]
 
 export const Prepaid = [
