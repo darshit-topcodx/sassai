731a79301bed61229b19bb4cbc69cb1af0ecb512
darshit-topcodx <darshit.topcodx@gmail.com>
2023-06-20 11:01:19 +0530

    create footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,51 +2,52 @@ import React from 'react'
 import './footer.scss'
 import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
 import TwitterIcon from '@mui/icons-material/Twitter';
-import SportsBasketballIcon from '@mui/icons-material/SportsBasketball';
 import InstagramIcon from '@mui/icons-material/Instagram';
-import TelegramIcon from '@mui/icons-material/Telegram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+
 const Footer = () => {
   return (
     <>
-      <div className='inbox-sec'>
-        <div className='ab-div'></div>
+      <div className='footer-sec'>
         <div className='container'>
-          <div className='inbox-content'>
-            <img src='https://i.pinimg.com/736x/fd/17/53/fd1753f50f60c9bf5363b1666f2cc5c0.jpg' style={{ width: '50px', height: '50px', borderRadius: '50%', marginBottom: '20px' }} />
-            <h2>
-              Get the best blog stories
-            </h2>
-            <h2>into your inbox!</h2>
-            <div className='email-box'>
-              <input placeholder='Enter Your Email Address' />
-              <button> <span><TelegramIcon /></span>SUBSCRIBE</button>
+          <div className='footer-content'>
+            <div className='ft-1'>
+              <h1>DAVINCI</h1>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi ut culpa maiores maxime illo nostrum aut totam, porro dolore minima</p>
+              <p>EN</p>
             </div>
-            <p className='check-box'><span><input type='checkbox' /></span>I agree to the Privacy Policy.</p>
+            <div className='ft-2'>
+              <p>Information</p>
+              <p>AI World</p>
+            </div>
+            <div className='ft-3'>
+              <p>Site Pages</p>
+              <p>Login</p>
+              <p>Register</p>
+            </div>
+            <div className='ft-4'>
+              <p>Company</p>
+              <p>Terms & Conditions</p>
+              <p>Privacy Policy</p>
+            </div >
+            <div className='ft-5'>
+              <p>Social Media</p>
+              <p>Follow up on social media to find out the latest updates.</p>
+              <div>
+                <FacebookOutlinedIcon />
+                <TwitterIcon />
+                <InstagramIcon />
+                <LinkedInIcon />
+              </div>
+              <p>Get Started Today</p>
+              <button>
+                SIGN UP NOW
+              </button>
+            </div>
           </div>
-        </div>
-        <div className='footers'>
-          <div className='container'>
-            <div className='footer-content'>
-              <p>
-                AncoraThemes © 2023. All Rights Reserved.
-              </p>
 
-              <div className='footer-links'>
-                <p className='cool-link'>Home</p>
-                <p className='cool-link'>Pages</p>
-                <p className='cool-link'>Protfolio</p>
-                <p className='cool-link'>Blog</p>
-                <p className='cool-link'>Contacts</p>
-              </div>
-              <div className='social-icons'>
-                <div><FacebookOutlinedIcon /></div>
-                <div><TwitterIcon /></div>
-                <div><SportsBasketballIcon /></div>
-                <div> <InstagramIcon /> </div>
-              </div>
-            </div>
-          </div>
         </div>
+
       </div>
     </>
   )
